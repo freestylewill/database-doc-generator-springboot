563046a06d5f52b6712555241c19d6cb8a302637
majun <freestylewill@163.com>
2020-07-02 15:45:43 +0800

    database-doc-generator-springboot初始化提交

---
+++ src/main/java/org/hackerandpainter/databasedocgenerator/bean/ColumnVo.java
@@ -0,0 +1,64 @@
+package org.hackerandpainter.databasedocgenerator.bean;
+
+/**
+ * ColumnVo
+ *
+ * @author zt
+ * @version 2018/10/6 0006
+ */
+public class ColumnVo {
+    private Integer id;
+    private String name;
+    private String type;
+    private String key;
+    private String isNullable;
+    private String comment;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getIsNullable() {
+        return isNullable;
+    }
+
+    public void setIsNullable(String isNullable) {
+        this.isNullable = isNullable;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
